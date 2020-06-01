c69fec7ac163dc271b304f89f3a2345afcf28abc
kanishk0210 <kanishkchoudhary02@gmail.com>
2020-06-01 17:49:41 +0530

    grunt setup 1

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $("#loginbtn").click(function(){
+        $("#loginmodal").modal("show");
+    });
+
+    $("#resbtn").click(function(){
+        $("#reservemodal").modal("show");
+    });
+
+    $("#mycarousel").carousel({interval:200});
+    $("#carouselButton").click(function(){
+        if($("#carouselButton").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        }
+        else if($("#carouselButton").children("span").hasClass("fa-play")){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }            
+    });                      
+});
